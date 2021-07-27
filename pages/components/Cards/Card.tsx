768b61d40a74e82c11bf6f35b998f4869e6825c2
Alyson Vilela <alysoonvila2@gmail.com>
2021-07-27 15:47:49 -0300

    Add styles

--- pages/components/Cards/Card.tsx
+++ pages/components/Cards/Card.tsx
@@ -1,20 +1,40 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 
 type Settings= {
   name: string,
   type: string,
-  bgImg: string
+  imgUrl: string,
+  price: string
 }
 
-export const Card = ({name, type, bgImg}: Settings) => {
+export const Card = ({name, imgUrl, price}: Settings) => {
+  const [ bgImg, setBgimg] = useState('')
+  const [ description, setDescription] = useState('');
+
+  useEffect(() => {
+    async function getUrl(url){
+      const response = await axios.get(url);
+      setBgimg(response.data.sprites.front_default);
+      setDescription(response.data.types.map((u) => u?.type.name));
+    }
+    getUrl(imgUrl)
+  }, [])
+
   return (
       <div className="row">
         <div className="card" style={{ backgroundImage: `url(${bgImg})` }}>
           <div className="text">
             <h3>{name}</h3>
             <div className="rate">
-              <span>{type}</span>
+              <div className="text-uppercase pb-2">
+              {description}
+              </div>
+              <div className="text-uppercase pb-2">
+
+              </div>
+              <button type="button" className="btn btn-dark">Add to cart</button>
               </div>
             </div>
           </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react';
 import IpApi from './api/ipapi';
 import PokeApi from './api/pokeapi';
 import { Card } from './components/Cards/Card';
@@ -11,16 +12,22 @@ export default function Home() {
   const { data , loading } =  PokeApi();
   const filter = data?.pokemon;
 
+  
+
 
   return (
     <div>
       <Navbar />
       {loading && <h1>Loading...</h1>}
       <div className="container">
+        <h1 className="my-5 text-uppercase text-bold">Pokemons for sale!</h1>
           <div className="col-9 d-flex flex-wrap justify-content-between gap-1">
       {filter?.map(pokedex => (
         <div className="col-2 mb-2">
-              <Card name={pokedex?.pokemon?.name} type={pokedex?.pokemon?.url} bgImg="https://static-cse.canva.com/blob/183499/IMAGE-1.jpg"/>
+              <Card name={pokedex?.pokemon?.name}
+              type={pokedex?.pokemon?.url}
+              imgUrl={pokedex?.pokemon?.url}
+              price={`${pokedex?.pokemon?.name.length * 25}`}/>
               </div>
       ))}
         </div>
