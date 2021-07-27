d49804df23465af754f01ec9ed4354277e2921d4
Alyson Vilela <alysoonvila2@gmail.com>
2021-07-27 19:59:18 -0300

    ip

--- pages/api/ipapi.tsx
+++ pages/api/ipapi.tsx
@@ -4,20 +4,17 @@ export const IP_API_URL = 'http://ip-api.com/json/?query=&fields=status,country,
 
 const IpApi = () => {
   const [ip, setIp] = useState(null);
-  const [loadingIp, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true)
     async function getIp() {
       const res = await fetch(IP_API_URL);
       const json = await res.json();
       setIp(json);
-      setLoading(false);
     }
     getIp()
   }, [])
 
-  return { ip, loadingIp, IP_API_URL }
+  return { ip, IP_API_URL }
 }
 
 export default IpApi
