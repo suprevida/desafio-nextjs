c0c601e1780bc9201db3624c6ae986c2fed494c7
Eduardo Cerqueira <eduardo.silva@saudeid.com.br>
2021-07-27 22:06:53 -0300

    resolve cors ip-api

--- next.config.js
+++ next.config.js
@@ -6,6 +6,6 @@ module.exports = {
   },
   env: {
     NEXT_PUBLIC_BASE_URL_POKEAPI: 'https://pokeapi.co/',
-    NEXT_PUBLIC_BASE_URL_IP_API: 'http://ip-api.com/',
+    NEXT_PUBLIC_BASE_URL_IP_API: 'https://ip-api.com/',
   }
 }
